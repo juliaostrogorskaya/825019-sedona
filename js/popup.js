30b7d4ac33e72026e8b6eb2b6f5390d6ce49dd1c
Julia <juliaostrogorskaya@gmail.com>
2018-09-17 16:34:25 +0700

    Финальная проверка

--- js/popup.js
+++ js/popup.js
@@ -6,6 +6,7 @@ var formDateDeparture = formPopup.querySelector("[name=departure]");
 var formAdultsNumber = formPopup.querySelector("[name=adults-number]");
 var formChildrenNumber = formPopup.querySelector("[name=children-number]");
 
+formPopup.classList.remove("hotel-search-show")
 formLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   formPopup.classList.toggle("hotel-search-show");
